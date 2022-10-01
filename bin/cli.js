4f0f084d5d841bcfef04b50e5bca604841b13ff0
dongguoqing <878744510@qq.com>
2022-10-01 15:19:26 +0800

    v2

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,7 @@
 const program = require('commander')
 const chalk = require('chalk')
 const ora = require('ora');
+const spawn = require('cross-spawn')
 
 // program.version('0.1.0').command('create <name>').description('create a project').action(name => {
 //     // 打印命令行输入的值
@@ -25,29 +26,55 @@ const ora = require('ora');
 // })
 // program.parse()
 
-// 自定义文本信息
-const message = 'Loading unicorns'
-// 初始化
-const spinner = ora(message);
-// 开始加载动画
-spinner.start();
+// // 自定义文本信息
+// const message = 'Loading unicorns'
+// // 初始化
+// const spinner = ora(message);
+// // 开始加载动画
+// spinner.start();
 
-setTimeout(() => {
-    // 修改动画样式
+// setTimeout(() => {
+//     // 修改动画样式
 
-    // Type: string
-    // Default: 'cyan'
-    // Values: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray'
-    spinner.color = 'red';    
-    spinner.text = 'Loading rainbows';
+//     // Type: string
+//     // Default: 'cyan'
+//     // Values: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray'
+//     spinner.color = 'red';    
+//     spinner.text = 'Loading rainbows';
 
-    setTimeout(() => {
-        // 加载状态修改
-        spinner.stop() // 停止
-        spinner.succeed('Loading succeed'); // 成功 ✔
-        // spinner.fail(text?);  失败 ✖
-        // spinner.warn(text?);  提示 ⚠
-        // spinner.info(text?);  信息 ℹ
-    }, 2000);
-}, 2000);
+//     setTimeout(() => {
+//         // 加载状态修改
+//         spinner.stop() // 停止
+//         spinner.succeed('Loading succeed'); // 成功 ✔
+//         // spinner.fail(text?);  失败 ✖
+//         // spinner.warn(text?);  提示 ⚠
+//         // spinner.info(text?);  信息 ℹ
+//     }, 2000);
+// }, 2000);
 
+
+
+
+//执行shell 的命令
+
+//定义需要的依赖
+const dependecies = ['vue', 'vuex', 'vue-router']
+
+//执行安装
+
+const child = spawn('yarn', ['add', '-D'].concat(dependecies), {
+    stdio:'inherit'
+})
+
+
+//监听执行结果
+
+child.on('close', code => {
+
+    if(code === 0){
+        //执行成功
+        console.log(chalk.cyan('install finished'))
+    }else{
+        console.log(chalk.red('下载依赖失败'))
+    }
+})
